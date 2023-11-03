5d0e4e10f365af6566c4e733df3de61cb588bcb1
Thiago NeveS <140116209+Thfneves@users.noreply.github.com>
2023-11-02 21:47:23 -0300

    v1
    Esse é o começo do projeto, nele ja inclui.
    *  as fotos dos personagens, botão de avançare continuar.
    *usando java inclui, a possibilidade de quando clicar em avançar mudar de foto e tambem para o sistema entender quando a proxima opção do avançar nao tem nenhuma informação. Sendo assim ele bloqueia a opçaode avançar, o mesmo para retrocesder.
    
    
    * Aqui tambem, após terminar esse projeto tentarei trazer alguma inovação, que não consta na aula que estou assistindo, para futuramente criar meus proprios projetos incluindo java.

---
+++ Script/index.js
@@ -0,0 +1,33 @@
+const members = [
+  { name: "Peter Quil", id: "peter" },
+  { name: "Gamora", id: "gamora" },
+  { name: "Drax o Destruidor", id: "drax" },
+  { name: "Mantis", id: "mantis" },
+  { name: "Rocket Raccoon", id: "rocket" },
+  { name: "I am Groot", id: "groot" },
+];
+
+let activeMember = 0;
+const images = document.getElementById("images");
+
+function changeStatusButtons() {
+  const prev = document.getElementById("button_prev");
+  const isFirst = activeMember === 0;
+  prev.disabled = isFirst;
+
+  const next = document.getElementById("button_next");
+  const isLast = activeMember == members.length - 1;
+  next.disabled = isLast;
+}
+
+function navigationMember(direction) {
+  activeMember = activeMember + direction;
+
+  const member = members[activeMember];
+
+  console.log(activeMember, member);
+
+  images.style.transform = `translateY(-${100 * activeMember}vh)`;
+
+  changeStatusButtons();
+}
